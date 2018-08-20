d91c376500e55fec381286e707967090e846eb96
Nikhil Miranda <41485519+nikhilmiranda@users.noreply.github.com>
2018-08-20 23:01:36 +0530

    Add files via upload

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Store Database",
+  "version": "1.0.0",
+  "description": " A custom REST api for virtual store along with it's Documentation",
+  "title": "Virtual Store",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-08-20T15:10:34.793Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
